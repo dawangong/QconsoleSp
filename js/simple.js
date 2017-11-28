f3e076bb9f74eaecb2f35fde31086ebbf7b52c23
dawangong <dawangong@126.com>
2017-11-28 10:43:04 +0800

    完善

--- js/simple.js
+++ js/simple.js
@@ -2,15 +2,17 @@ let cons=document.getElementById('console');
 let cle=document.getElementById('clear');
 let hid=document.getElementById('hide');
 let content=document.getElementById('content');
-
+let sr=document.getElementById('sr');
 //控制效果
 cons.onclick=() => {
     cle.style.display='block';
     hid.style.display='block';
+    sr.style.display='block';
 };
 hid.onclick=() => {
     cle.style.display='none';
     hid.style.display='none';
+    sr.style.display='none';
 };
 //清除效果
 cle.onclick=() => {
@@ -47,4 +49,24 @@ console.debug = function(str){
 
 console.warn = function(str){
     control(str);
+};
+
+//判断模拟输入，同时传入实参调用对应函数
+let vl=document.getElementById('vl');
+document.onkeydown=function(ev){
+    let e=ev||event;
+    if(e.keyCode==13){
+        let lf=vl.value.indexOf('(');
+        let rh=vl.value.lastIndexOf(')');
+        let pd=vl.value.slice(lf-3,lf);
+        let cs=vl.value.slice(lf+1,rh);
+        vl.value='';
+        switch (pd){
+            case 'log':console.log(eval(cs));break;
+            case 'nfo':console.info(eval(cs));break;
+            case 'ror':console.error(eval(cs));break;
+            case 'bug':console.debug(eval(cs));break;
+            case 'arn':console.warn(eval(cs));break;
+        }
+    }
 };
